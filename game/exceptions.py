7a150c1ad51f3734b0d2b4e2bb672ce928da99a6
gggosle <gakhovalera2004@gmail.com>
2026-04-20 10:53:32 +0300

    Remove unnecessary exception comments

--- game/exceptions.py
+++ game/exceptions.py
@@ -1,7 +1,5 @@
 class GameDomainError(Exception):
-    """Base exception for all game-related domain errors."""
     pass
 
 class InvalidMoveError(GameDomainError):
-    """Raised when a player attempts an illegal Checkers move."""
     pass
